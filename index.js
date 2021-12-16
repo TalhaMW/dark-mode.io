17c3a0a39d6aab81e0b6278b6381c63f19879e19
TalhaMW <88961097+TalhaMW@users.noreply.github.com>
2021-12-16 10:35:19 +0500

    dark mode first repo

---
+++ index.js
@@ -0,0 +1,22 @@
+let btn = document.querySelector('.toggle-btn')
+let icon = document.querySelector('i')
+let modeText = document.querySelector('.mode-text');
+let link = document.querySelector('#css');
+let img = document.querySelectorAll('img')
+let darkMode = false;
+btn.addEventListener('click',()=>{
+    if(darkMode===false){ 
+        modeProps("href","sass/dark-mode.css",true,"fas fa-moon","Dark Mode","dark-mode-img/img-")
+
+    }else if(darkMode===true){
+        modeProps("href","sass/index.css",false,"fas fa-sun","Light Mode","/light-mode-img/img-")
+    }
+})
+
+function modeProps(styleAttribute,attributeValue,darkModeLogic,iconClass,modeName,imgSrc){
+    link.setAttribute(styleAttribute,attributeValue)
+    darkMode=darkModeLogic;
+    icon.className=iconClass;
+    modeText.textContent=modeName
+    img.forEach((item,index)=>{item.src=imgSrc+`${index+1}.png`;})
+}
